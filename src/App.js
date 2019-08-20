b9a1ea5dc6453d81d65cd2076872fb680b91fe38
liuhaitian <liuhaitian@bytedance.com>
2019-08-20 22:57:05 +0800

    feat: UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import anime from 'animejs/lib/anime.es.js';
 
-class App extends Component {
-  componentDidMount() {
+function App() {
+  useEffect(() => {
     const staggerVisualizerEl = document.querySelector('.stagger-visualizer');
     const fragment = document.createDocumentFragment();
-    const grid = [17, 17];
+    const grid = [11, 11];
     const col = grid[0];
     const row = grid[1];
     const numberOfElements = col * row;
@@ -16,63 +16,99 @@ class App extends Component {
 
     staggerVisualizerEl.appendChild(fragment);
 
-    const staggersAnimation = anime.timeline({
-      targets: '.stagger-visualizer div',
-      easing: 'easeInOutSine',
-      delay: anime.stagger(50),
-      loop: true,
-      autoplay: false
-    })
-    .add({
-      translateX: [
-        {value: anime.stagger('-.1rem', {grid: grid, from: 'center', axis: 'x'}) },
-        {value: anime.stagger('.1rem', {grid: grid, from: 'center', axis: 'x'}) }
-      ],
-      translateY: [
-        {value: anime.stagger('-.1rem', {grid: grid, from: 'center', axis: 'y'}) },
-        {value: anime.stagger('.1rem', {grid: grid, from: 'center', axis: 'y'}) }
-      ],
-      duration: 1000,
-      scale: .5,
-      delay: anime.stagger(100, {grid: grid, from: 'center'})
-    })
-    .add({
-      translateX: () => anime.random(-10, 10),
-      translateY: () => anime.random(-10, 10),
-      delay: anime.stagger(8, {from: 'last'})
-    })
-    .add({
-      translateX: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'x'}),
-      translateY: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'y'}),
-      rotate: 0,
-      scaleX: 2.5,
-      scaleY: .25,
-      delay: anime.stagger(4, {from: 'center'})
-    })
-    .add({
-      rotate: anime.stagger([90, 0], {grid: grid, from: 'center'}),
-      delay: anime.stagger(50, {grid: grid, from: 'center'})
-    })
-    .add({
-      translateX: 0,
-      translateY: 0,
-      scale: .5,
-      scaleX: 1,
-      rotate: 180,
-      duration: 1000,
-      delay: anime.stagger(100, {grid: grid, from: 'center'})
-    })
-    .add({
-      scaleY: 1,
-      scale: 1,
-      delay: anime.stagger(20, {grid: grid, from: 'center'})
-    })
+    const staggersAnimation = anime
+      .timeline({
+        targets: '.stagger-visualizer div',
+        easing: 'easeInOutSine',
+        delay: anime.stagger(50),
+        loop: true,
+        autoplay: false,
+      })
+      .add({
+        translateX: [
+          {
+            value: anime.stagger('-.1rem', {
+              grid: grid,
+              from: 'center',
+              axis: 'x',
+            }),
+          },
+          {
+            value: anime.stagger('.1rem', {
+              grid: grid,
+              from: 'center',
+              axis: 'x',
+            }),
+          },
+        ],
+        translateY: [
+          {
+            value: anime.stagger('-.1rem', {
+              grid: grid,
+              from: 'center',
+              axis: 'y',
+            }),
+          },
+          {
+            value: anime.stagger('.1rem', {
+              grid: grid,
+              from: 'center',
+              axis: 'y',
+            }),
+          },
+        ],
+        duration: 1000,
+        scale: 0.5,
+        delay: anime.stagger(100, { grid: grid, from: 'center' }),
+      })
+      .add({
+        translateX: () => anime.random(-10, 10),
+        translateY: () => anime.random(-10, 10),
+        delay: anime.stagger(8, { from: 'last' }),
+      })
+      .add({
+        translateX: anime.stagger('.25rem', {
+          grid: grid,
+          from: 'center',
+          axis: 'x',
+        }),
+        translateY: anime.stagger('.25rem', {
+          grid: grid,
+          from: 'center',
+          axis: 'y',
+        }),
+        rotate: 0,
+        scaleX: 2.5,
+        scaleY: 0.25,
+        delay: anime.stagger(4, { from: 'center' }),
+      })
+      .add({
+        rotate: anime.stagger([90, 0], { grid: grid, from: 'center' }),
+        delay: anime.stagger(50, { grid: grid, from: 'center' }),
+      })
+      .add({
+        translateX: 0,
+        translateY: 0,
+        scale: 0.5,
+        scaleX: 1,
+        rotate: 180,
+        duration: 1000,
+        delay: anime.stagger(100, { grid: grid, from: 'center' }),
+      })
+      .add({
+        scaleY: 1,
+        scale: 1,
+        delay: anime.stagger(20, { grid: grid, from: 'center' }),
+      });
 
     staggersAnimation.play();
-  }
+  });
 
-  render() {
-    return <div class="stagger-visualizer"></div>;
-  }
+  return (
+    <>
+      <h1 className="title">SHANISM</h1>
+      <div className="stagger-visualizer" />
+    </>
+  );
 }
 export default App;
